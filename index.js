a2b292c30393c931bc5e4a2f371d29c93e2f62e3
genius8545 <ahmedabdelsalm6@gmail.com>
2023-03-16 21:50:28 +0200

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#image-form");
+const resultDiv = document.querySelector("#result");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData();
+  formData.append("image", form.elements.image.files[0]);
+  try {
+    const res = await fetch("https://excited-bee-buckle.cyclic.app/ocr", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    console.log(data);
+    resultDiv.textContent = data.ocrResult;
+  } catch (err) {
+    console.error(err);
+    resultDiv.textContent = "Error occurred";
+  }
+});
